969a9dd2dc2247b980aec8daa614213ecd1d4411
albieduffy <albie.duffy@gmail.com>
2020-07-27 12:40:19 +0100

    Update comments access

--- app.js
+++ app.js
@@ -7,42 +7,43 @@ const fs = require('fs-extra');
 app.use(cors());
 app.use(bodyParser.text());
 
+// Access external data storage
+
 let rawData = fs.readFileSync('blogs.json');
 let blogs = JSON.parse(rawData);
 let blogID;
 
+// Access client side files
 app.use(express.static('client'));
 
+//Home route
 app.get('/', (req, res) => res.send('Hello world!'))
 
+//All blogs route
 app.get('/blogs', (req, res) => res.send(JSON.stringify(blogs)));
 
+// New blog post route
 app.post('/blogs/new', (req, res) => {
   const newPost = req.body;
-  blogs.push(newPost);
+  blogs.blogs.push(newPost);
   res.send(JSON.stringify(newPost));
 });
 
-app.get('/blog/:id/comments', (req, res) => {
-  blogID = req.params.id;
-  for (blog in blogs) {
-    if (blogs.blogs.key === blogID) {
-      res.send(JSON.stringify(blog.comments));
-    }
-  }
+// Access comments
+app.get('/blogs/:id/comments', (req, res) => {
+  blogID = Number(req.params.id);
+  res.send(JSON.stringify(blogs.blogs[blogID].comments))
 });
 
+// Post new comment
 app.post('/blogs/:id/comments', (req, res) => {
   const newComment = req.body;
   blogID = req.params.id;
-  for (blog in blogs) {
-    if (blogs.blogs.key === blogID) {
-      blogs.comments.push(newComment);
-    }
-  }
-  res.send(JSON.stringify(newComment))
+  blogs.blogs[blogID].comments.push(newComment);
+  res.send(JSON.stringify(newComment));
 });
 
+// Search through blog posts
 app.get('/blogs/search', (req, res) => {
   let searchTerm = req.query.q;
   let results = blogSearch(searchTerm);
@@ -53,6 +54,7 @@ app.get('/blogs/search', (req, res) => {
 
 app.listen(process.env.PORT || 3000, () => console.log(`Express now departing!`));
 
+// Blog search function
 const blogSearch = (searchTerm) => {
   return blogs.blogs.filter(
     (blog) =>
