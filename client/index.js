48c99a27d7d2ad6af858da6733242dff423e4a1f
georgeo23 <olney.george@yahoo.co.uk>
2020-07-27 12:04:08 +0100

    added 'add comment' which hides on load

--- client/index.js
+++ client/index.js
@@ -1,15 +1,31 @@
-// let newPostSection;
-// let addNew;
-// newPostSection = document.querySelector('.new-post')
-// addNew = document.querySelector('#addNew')
-// addNew.addEventListener('click', showNewPost)
-// hideNewPost()
-//
-//
-// function hideNewPost() {
-//   newPostSection.setAttribute('style', 'visibility: hidden;')
-// }
-//
-// function showNewPost() {
-//   newPostSection.setAttribute('style', 'visibility: visible;')
-// }
+let newPostSection;
+let addNew;
+let commentSection;
+newPostSection = document.querySelector('.new-post')
+commentSection = document.querySelector('.comment-section')
+addNew = document.querySelector('#addNew')
+addNew.addEventListener('click', showNewPost)
+hideNewPost()
+hideCommentSection()
+loadBlogs()
+
+function loadBlogs() {
+  fetch('http://localhost:3000/blogs')
+    .then(r => r.JSON())
+    .then(drawBlogs)
+    .catch(console.error())
+}
+
+function drawBlogs(blog) {
+  console.log(blog)
+}
+function hideNewPost() {
+  newPostSection.setAttribute('style', 'visibility: hidden;')
+}
+
+function hideCommentSection() {
+  commentSection.setAttribute('style', 'visibility: hidden;')
+}
+function showNewPost() {
+  newPostSection.setAttribute('style', 'visibility: visible;')
+}
