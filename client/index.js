0731e5e4d16eb733d29a816d442127158856d740
georgeo23 <olney.george@yahoo.co.uk>
2020-07-27 11:23:46 +0100

    added 'post box' html and css

---
+++ client/index.js
@@ -0,0 +1,15 @@
+// let newPostSection;
+// let addNew;
+// newPostSection = document.querySelector('.new-post')
+// addNew = document.querySelector('#addNew')
+// addNew.addEventListener('click', showNewPost)
+// hideNewPost()
+//
+//
+// function hideNewPost() {
+//   newPostSection.setAttribute('style', 'visibility: hidden;')
+// }
+//
+// function showNewPost() {
+//   newPostSection.setAttribute('style', 'visibility: visible;')
+// }
